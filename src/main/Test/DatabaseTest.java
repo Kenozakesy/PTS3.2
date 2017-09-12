03aeef63c838e0be82e1e91057832bd4d9747c59
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-09-12 09:34:53 +0200

    opstart

---
+++ src/main/Test/DatabaseTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+/**
+ * Created by Gebruiker on 12-9-2017.
+ */
+
+
+public class DatabaseTest {
+
+    @Test
+    public void Test()
+    {
+
+
+
+    }
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+
+        Scene scene = new Scene(root, Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
