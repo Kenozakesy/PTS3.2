527af9c3c22cf61248258d75d531826dbb169b4d
Jordi <j.vandermeij@student.fontys.nl>
2017-09-19 13:42:35 +0200

    Quick commit

--- src/main/java/Business/Player.java
+++ src/main/java/Business/Player.java
@@ -2,14 +2,18 @@ package Business;
 
 import Business.Enums.Role;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jordi on 12-9-2017.
  */
 public class Player {
 
-    String name;
-    int points;
-    Role role;
+    private String name;
+    private int points;
+    private Role role;
+    private List<PlayCard> cardsInHand;
 
     public String getName(){return name;}
     public void setName(String name){this.name = name;}
@@ -17,10 +21,21 @@ public class Player {
     public void setPoints(int n){this.points = n;}
     public Role getRole(){return role;}
     public void setRole(Role n){this.role = n;}
+    public List<PlayCard> getCardsInHand() {return cardsInHand;}
 
-    public Player(String name){
+    public Player(String name)
+    {
         this.name = name;
+        cardsInHand = new ArrayList<PlayCard>();
     }
+
+    public void addToHand(PlayCard card)
+    {
+        cardsInHand.add(card);
+    }
+
+
+
 
 
 }
