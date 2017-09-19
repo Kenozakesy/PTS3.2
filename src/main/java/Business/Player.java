5a2db14d343b03bfa9fde3e9a5321d7300624176
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-09-19 09:10:39 +0200

    player class update

--- src/main/java/Business/Player.java
+++ src/main/java/Business/Player.java
@@ -1,5 +1,7 @@
 package Business;
 
+import Business.Enums.Role;
+
 /**
  * Created by Jordi on 12-9-2017.
  */
@@ -7,12 +9,18 @@ public class Player {
 
     String name;
     int points;
+    Role role;
+
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+    public int getPoints(){return points;}
+    public void setPoints(int n){this.points = n;}
+    public Role getRole(){return role;}
+    public void setRole(Role n){this.role = n;}
 
     public Player(String name){
         this.name = name;
     }
 
-    public String getName(){return name;}
-    public void setName(String name){this.name = name;}
-    public int getPoints(){return points;}
+
 }
