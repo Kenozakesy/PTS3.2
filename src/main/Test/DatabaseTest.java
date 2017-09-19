03d06653df69fb9a55f0aeb7fd8b39808e0bf71f
Jelle Schrader <jelleschrader@gmail.com>
2017-09-19 09:12:26 +0200

    idk, commit
    Something something something darkside

--- src/main/Test/DatabaseTest.java
+++ src/main/Test/DatabaseTest.java
@@ -1,19 +1,53 @@
 import org.junit.Test;
 
+import java.sql.*;
+import java.util.Properties;
+
 /**
  * Created by Gebruiker on 12-9-2017.
  */
 
 
 public class DatabaseTest {
+    private Connection connection;
+    private Properties properties;
+    private String user = "dbi299244";
+    private String pass = "PTS3Groep1";
+            //"Server=mssql.fhict.local;Database=dbi299244;User Id=dbi299244;Password=PTS3Groep1;";
 
-    @Test
-    public void Test()
-    {
-
-
-
+    private Properties GetProperties() {
+        try {
+            if (properties == null) {
+                properties = new Properties();
+                properties.setProperty("user", user);
+                properties.setProperty("password", pass);
+            }
+            return properties;
+        }
+        catch(Exception exception){
+            exception.printStackTrace();
+            return null;
+        }
     }
 
+    public static Connection Connect(){
+        try{
+            return DriverManager.getConnection("Server=mssql.fhict.local;Database=dbi299244;", "dbi299244", "PTS3Groep1");
+        }
+        catch(Exception exception){
+            exception.printStackTrace();
+            return null;
+        }
+    }
+    public void Disconnect(){
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public
 
 }
