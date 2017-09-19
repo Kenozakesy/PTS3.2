5a2445db6950bfa270c180e1c0ce147d5c66a7bd
Wartenberg,Koen K.W <k.wartenberg@student.fontys.nl>
2017-09-19 11:36:13 +0200

    startGameControllerUpdate

---
+++ src/main/java/Business/staticClasses/StaticPlayer.java
@@ -0,0 +1,27 @@
+package Business.staticClasses;
+
+
+import Business.Enums.*;
+
+/**
+ * Created by Gebruiker on 19-9-2017.
+ */
+public class StaticPlayer {
+
+    private static String name = "Test player";
+    private static Integer points;
+    private static Role role = Role.Nothing;
+
+    public static String getName() {return name;}
+    public static void setName(String name) {StaticPlayer.name = name;}
+
+    public static Integer getPoints() {return points;}
+    public static void setPoints(Integer points) {StaticPlayer.points = points;}
+
+    public static Role getRole() {return role;}
+    public static void setRole(Role role) {StaticPlayer.role = role;}
+
+
+
+
+}
--- src/main/java/StartGameController.java
+++ src/main/java/StartGameController.java
@@ -1,8 +1,23 @@
+import Business.staticClasses.StaticLobby;
+import Business.staticClasses.StaticPlayer;
+import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Gebruiker on 12-9-2017.
@@ -12,7 +27,10 @@ import javafx.scene.control.TextField;
 public class StartGameController {
 
     @FXML
-    private ComboBox ddScoreLimit;
+    public Button btnStartGame;
+
+    @FXML
+    private ComboBox ddScorelimit;
 
     @FXML
     private ComboBox ddPlayerLimit;
@@ -35,13 +53,19 @@ public class StartGameController {
     @FXML
     private void btnSend(Event e)
     {
-        //send chat messages
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        //get current date time with Date()
+        Date date = new Date();
+
+        String Text = tfChatBox.getText();
+        taChat.appendText(dateFormat.format(date) + " " + StaticPlayer.getName() +": "+ Text + "\n");
+        tfChatBox.setText("");
     }
 
     @FXML
     private void btnChangeNickName(Event e)
     {
-        //changes your current nickname
+        //not implemented
     }
 
     @FXML
@@ -51,8 +75,33 @@ public class StartGameController {
     }
 
     @FXML
-    private void btnStartGame(Event e)
+    private void btnStartGame(Event e) throws Exception
     {
+        //goes to different view
         //starts the game with current options
+        Stage stage = (Stage) btnStartGame.getScene().getWindow();
+        stage.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GameView.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage2 = new Stage();
+        stage2.setScene(new Scene(root1)); stage2.show();
+    }
+
+    @FXML
+    private void UpdateGameOptions(Event e)
+    {
+        StaticLobby.setScorelimit(Integer.parseInt((String)ddScorelimit.getSelectionModel().getSelectedItem()));
+        StaticLobby.setMaxplayers(Integer.parseInt((String)ddPlayerLimit.getSelectionModel().getSelectedItem()));
+        StaticLobby.setMaxspectators(Integer.parseInt((String)ddSpectatorLimit.getSelectionModel().getSelectedItem()));
+        StaticLobby.setTimelimit((String)ddIdleTimer.getSelectionModel().getSelectedItem());
+        StaticLobby.setBlankcards(Integer.parseInt((String)ddBlankCards.getSelectionModel().getSelectedItem()));
+
+       // only used for testing
+//        taChat.setText(String.valueOf(StaticLobby.getScorelimit()) + "\n" +
+//                        String.valueOf(StaticLobby.getMaxplayers()) + "\n" +
+//                        String.valueOf(StaticLobby.getMaxspectators()) + "\n" +
+//                        String.valueOf(StaticLobby.getTimelimit()) + "\n" +
+//                        String.valueOf(StaticLobby.getBlankcards()));
     }
 }
