35e79686bf566b3b5f53ab7461e1675acd0a6ca8
Jordi <j.vandermeij@student.fontys.nl>
2017-09-26 11:02:31 +0200

    Barely a commit
    To avoid conflicts with though

--- src/main/java/Business/Game.java
+++ src/main/java/Business/Game.java
@@ -4,15 +4,28 @@ package Business;
  * Created by Jordi on 26-9-2017.
  */
 
+import java.util.Random;
+
 /**
  * In Sprint 2 wordt deze klasse volledig uitgewerkt
  */
 public class Game
 {
+    Random random = new Random();
     Game(){}
 
     public void endTurn()
     {
+        // for(Player player : playerList)
+        // {
+        //    while(player.cardsInHand < 8)
+        //  {
+        //    int index = random.nextInt(playcardsList.size());
+        //    player.addCard(playcardsList.get(index));
+        // }
+        //
+        // }
+        //
 
     }
 
