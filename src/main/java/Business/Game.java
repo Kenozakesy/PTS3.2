629f0d95bc9c30ae7cb40d77569a0946d78069dc
Jordi <j.vandermeij@student.fontys.nl>
2017-09-26 09:48:32 +0200

    Game class added for future use

---
+++ src/main/java/Business/Game.java
@@ -0,0 +1,23 @@
+package Business;
+
+/**
+ * Created by Jordi on 26-9-2017.
+ */
+
+/**
+ * In Sprint 2 wordt deze klasse volledig uitgewerkt
+ */
+public class Game
+{
+    Game(){}
+
+    public void endTurn()
+    {
+
+    }
+
+
+
+}
+
+
