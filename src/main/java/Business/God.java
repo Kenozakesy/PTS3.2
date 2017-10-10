c009a41719cf0661bef641bba783ed0bdeafb27f
ImJellyUJelly <jelleschrader@gmail.com>
2017-10-10 09:53:26 +0200

    Eroors God fixed
    Errors in god gefixt, ik push dit keer mn workspace niet meer, kijk Arjan ziet dat ie is uitgevingt.

--- src/main/java/Business/God.java
+++ src/main/java/Business/God.java
@@ -1,6 +1,7 @@
 package Business;
 
 import Business.Enums.Status;
+import com.sun.media.jfxmedia.events.PlayerStateEvent;
 
 import java.util.ArrayList;
 
@@ -12,26 +13,27 @@ public class God {
         lobbies = new ArrayList<>();
     }
 
-    // Creeert een lobby;
+    // Creeert een lobby
     public void createLobby(int id, int ip, int maxPlayers, int maxSpectators, int scoreLimit, int blankCards, String timeLimit, String password, String status) {
         String lobbyId = String.valueOf(id);
         String hostIp = String.valueOf(ip);
         Lobby lobby = new Lobby(lobbyId, hostIp);
+        lobbies.add(lobby);
     }
 
-    // Start de game, met de lobby van het meegegeven id;
+    // Start de game, met de lobby van het meegegeven id
     public void startGame(int id) {
         String lobbyId = String.valueOf(id);
         for(Lobby lobby : lobbies) {
-            if(lobby.getLobbyID() == lobbyId) {
+            if(lobby.getLobbyID().equals(lobbyId)) {
                 lobby.startGame();
             }
         }
     }
 
-    // Returnt alle bestaande lobbies, die niet gestart zijn;
+    // Returnt alle bestaande lobbies, die niet gestart zijn
     public ArrayList<Lobby> getLobbiesNotStarted() {
-        ArrayList<Lobby> lobbiesNotStarted = new ArrayList<Lobby>();
+        ArrayList<Lobby> lobbiesNotStarted = new ArrayList<>();
         for(Lobby lobby : lobbies) {
             if(lobby.getStatus() == Status.Not_started) {
                 lobbiesNotStarted.add(lobby);
@@ -40,18 +42,23 @@ public class God {
         return lobbiesNotStarted;
     }
 
-    public ArrayList<Player> getPlayersFromLobby() {
-
+    // Returnt de spelers uit een bepaalde lobby
+    public ArrayList<Player> getPlayersFromLobby(int lobbyId) {
+        Lobby lobby = getLobbyFromId(lobbyId);
+        ArrayList<Player> players = new ArrayList<>();
+        players.addAll(lobby.getPlayers());
+        return players;
     }
 
-    // Deze method is om een lobby uit de lijst met lobbies te vissen met een bepaald Id;
+    // Deze method is om een lobby uit de lijst met lobbies te vissen met een bepaald Id
     private Lobby getLobbyFromId(int id) {
         String lobbyId = String.valueOf(id);
         for(Lobby lobby : lobbies) {
-            if(lobby.getLobbyID() == lobbyId) {
+            if(lobby.getLobbyID().equals(lobbyId)) {
                 return lobby;
             }
         }
+        return null;
     }
 
 }
