c4dff2405743e888ac5888e6a9a94942ef5889ca
ImJellyUJelly <jelleschrader@gmail.com>
2017-10-10 09:40:26 +0200

    God toevoegingen
    createLobby, startGame, getLobbyById, getPlayersFromLobby

--- src/main/java/Business/Enums/Status.java
+++ src/main/java/Business/Enums/Status.java
@@ -2,7 +2,7 @@ package Business.Enums;
 
 /**
  * Created by Gebruiker on 12-9-2017.
- */
+         */
 public enum Status {
 
     In_progress,
--- src/main/java/Business/God.java
+++ src/main/java/Business/God.java
@@ -1,8 +1,57 @@
 package Business;
 
+import Business.Enums.Status;
+
+import java.util.ArrayList;
+
+// FACADE
 public class God {
+    private ArrayList<Lobby> lobbies;
+
     public God() {
+        lobbies = new ArrayList<>();
+    }
+
+    // Creeert een lobby;
+    public void createLobby(int id, int ip, int maxPlayers, int maxSpectators, int scoreLimit, int blankCards, String timeLimit, String password, String status) {
+        String lobbyId = String.valueOf(id);
+        String hostIp = String.valueOf(ip);
+        Lobby lobby = new Lobby(lobbyId, hostIp);
+    }
+
+    // Start de game, met de lobby van het meegegeven id;
+    public void startGame(int id) {
+        String lobbyId = String.valueOf(id);
+        for(Lobby lobby : lobbies) {
+            if(lobby.getLobbyID() == lobbyId) {
+                lobby.startGame();
+            }
+        }
+    }
+
+    // Returnt alle bestaande lobbies, die niet gestart zijn;
+    public ArrayList<Lobby> getLobbiesNotStarted() {
+        ArrayList<Lobby> lobbiesNotStarted = new ArrayList<Lobby>();
+        for(Lobby lobby : lobbies) {
+            if(lobby.getStatus() == Status.Not_started) {
+                lobbiesNotStarted.add(lobby);
+            }
+        }
+        return lobbiesNotStarted;
+    }
+
+    public ArrayList<Player> getPlayersFromLobby() {
 
     }
-    
+
+    // Deze method is om een lobby uit de lijst met lobbies te vissen met een bepaald Id;
+    private Lobby getLobbyFromId(int id) {
+        String lobbyId = String.valueOf(id);
+        for(Lobby lobby : lobbies) {
+            if(lobby.getLobbyID() == lobbyId) {
+                return lobby;
+            }
+        }
+    }
+
 }
